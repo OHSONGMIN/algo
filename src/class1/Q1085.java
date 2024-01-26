63933c18cd7bd054f8d8bfa6e7f19d8cb61b6df7
SONGMIN <dhthdals@gmail.com>
2024-01-26 23:46:53 +0900

    1/26 백준

---
+++ src/class1/Q1085.java
@@ -0,0 +1,27 @@
+package class1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int[] arr = new int[4];
+
+        arr[0] = w - 0;
+        arr[1] = h - 0;
+        arr[2] = x - w;
+        arr[3] = y - h;
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0]);
+
+    }
+}
