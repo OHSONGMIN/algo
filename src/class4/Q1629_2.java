16d62268918fdffe6eadc9fed5e1c924ca2dbad5
OHSONGMIN <143185293+OHSONGMIN@users.noreply.github.com>
2024-04-25 23:50:45 +0900

    4/25 백준

--- src/class4/Q1629_2.java
+++ src/class4/Q1629_2.java
@@ -3,17 +3,25 @@ package class4;
 import java.util.Scanner;
 
 public class Q1629_2 {
+    static long C;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int result = A;
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        C = sc.nextLong();
 
-        if (B % 2 == 0) { //짝수라면
+        System.out.println(pow(A, B));
+    }
 
+    private static long pow(long A, long exponent ) {
+
+        if (exponent  == 1) return A % C;
+
+        long temp = pow(A, exponent  / 2);
+
+        if (exponent  % 2 == 1) {
+            return ((temp * temp % C) * (A % C)) % C;
         }
-
-        System.out.println(result);
+        return temp * temp % C;
     }
 }
