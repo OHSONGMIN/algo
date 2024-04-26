9ea7106703e4bc651b8157016adb06f88488cd25
OHSONGMIN <143185293+OHSONGMIN@users.noreply.github.com>
2024-04-27 00:34:20 +0900

    4/27 백준

--- src/class4/Q1932_2.java
+++ src/class4/Q1932_2.java
@@ -1,4 +1,43 @@
 package class4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q1932_2 {
+    static int[][] arr;
+    static Integer[][] dp;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        dp = new Integer[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[N - 1][i] = arr[N - 1][i];
+        }
+
+        System.out.println(findDp(0, 0));
+    }
+
+    private static int findDp(int i, int j) {
+
+        if (dp[0][0] != null) return dp[0][0];
+
+        if (dp[i][j] == null) {
+
+            dp[i][j] = arr[i][j] + Math.max(findDp(i + 1, j), findDp(i + 1, j + 1));
+        }
+        return dp[i][j];
+    }
 }
---
+++ src/class4/Q1991.java
@@ -0,0 +1,14 @@
+package class4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1991 {
+    static int N;
+    static String[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+}
